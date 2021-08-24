2b61a806f136584c8b72d911508257650cc4d16a
gaspoltech <88130246+gaspoltech@users.noreply.github.com>
2021-08-25 01:21:54 +0700

    Update vishanduser.py

--- vishanduser.py
+++ vishanduser.py
@@ -39,11 +39,11 @@ def main():
     # elif choice == "Sasaran Penerima Bantuan":
     elif choice == 'Assessment & Recommendations':
         st.subheader('Eligibily Check And Efficiency Strategy Recommendation')
-        provinsi = st.sidebar.selectbox('Province',df.Prov.unique())
+        provinsi = st.selectbox('Province',df.Prov.unique())
         df = df[df['Prov'].isin([provinsi])]
-        pemda = st.sidebar.selectbox('City',df.Kab_APBD.unique())
+        pemda = st.selectbox('City',df.Kab_APBD.unique())
         df = df[df['Kab_APBD'].isin([pemda])]
-        umkm = st.sidebar.selectbox('Choose MSME Name',['All']+df.BU.unique().tolist())
+        umkm = st.selectbox('Choose MSME Name',['All']+df.BU.unique().tolist())
         st.title(umkm)
         if umkm=='All':
             st.write('Please Choose MSME')
